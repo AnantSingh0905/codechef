9343e1d9957cec128c4679ae8878ca3503963f1b
AnantSingh0905 <104147289+AnantSingh0905@users.noreply.github.com>
2023-04-26 17:39:17 +0530

    Update Chef and his Apps.py

--- Chef and his Apps.py
+++ Chef and his Apps.py
@@ -3,9 +3,9 @@ num = int(input())
 for i in range (num):
     a,b,c,d=[int(x) for x in input().split()]
     e=a-b-c
-    if (e)>d:
+    if (e)>=d:
         print("0")
-    elif (e+b)>d or (e+c)>d:
+    elif (e+b)>=d or (e+c)>=d:
         print("1")
     else:
         print("2")
